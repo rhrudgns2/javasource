badabb2984a848a10f9233788db105a0ecebcf55
kyunghoon <kokd2008@naver.com>
2020-11-03 15:16:34 +0900

    오류 제거

--- 10Exception/src/exception/ExceptionTest2.java
+++ 10Exception/src/exception/ExceptionTest2.java
@@ -3,12 +3,12 @@ public class ExceptionTest2 {
 
 	public static void main(String[] args) {
 		String data1 = "100";
-		String data2 = "a100";
+		String data2 = "200";
 		int value1 =Integer.parseInt(data1);
 		int value2 =Integer.parseInt(data2);
 		
 		System.out.println(value1 + value2);
-		
+		System.out.println(value1);
 	}
 
 }
