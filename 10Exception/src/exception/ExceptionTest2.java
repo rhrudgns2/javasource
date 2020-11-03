f90ffcf0724ef2cfd222d417efde873d754e83b0
kyunghoon <kokd2008@naver.com>
2020-11-03 15:02:27 +0900

    소스수정

---
+++ 10Exception/src/exception/ExceptionTest2.java
@@ -0,0 +1,14 @@
+package exception;
+public class ExceptionTest2 {
+
+	public static void main(String[] args) {
+		String data1 = "100";
+		String data2 = "a100";
+		int value1 =Integer.parseInt(data1);
+		int value2 =Integer.parseInt(data2);
+		
+		System.out.println(value1 + value2);
+		
+	}
+
+}
